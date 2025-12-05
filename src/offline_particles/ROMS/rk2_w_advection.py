64ed6ba00d6584f8a5a0bde49b0dde40374801ee
Jamie Hilditch <hilditch@stanford.edu>
2025-12-04 21:57:42 -0800

    run compute_zidx

--- src/offline_particles/ROMS/rk2_w_advection.py
+++ src/offline_particles/ROMS/rk2_w_advection.py
@@ -150,8 +150,8 @@ def _rk2_step_2(
     zeta_value = bilinear_interpolation(zeta_idx, zeta)
     
     S = S_from_z(z, h_value, zeta_value)
-    return S
-    # zidx = compute_zidx_from_S(S, hc, NZ, h_value, zeta_value, C, C_off)
+    zidx = compute_zidx_from_S(S, hc, NZ, h_value, zeta_value, C, C_off)
+    return zidx
     
     # # horizontal advection in index space
     # # offset indices for u, v, dx, dy

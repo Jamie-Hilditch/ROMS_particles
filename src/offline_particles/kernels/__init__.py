17c6eea6c3453a77e1cc884c07ae3875d4b934af
Jamie Hilditch <hilditch@stanford.edu>
2025-12-07 21:32:24 -0800

    create interpolation kernels

--- src/offline_particles/kernels/__init__.py
+++ src/offline_particles/kernels/__init__.py
@@ -1,7 +1,15 @@
 """Predefined kernels."""
 
+from .interpolation import (
+    create_horizontal_bilinear_interpolation_kernel,
+    create_trilinear_interpolation_kernel,
+)
 from .validation import finite_indices_kernel, inbounds_kernel, validate_indices_kernel
 
 __all__ = [
-    "finite_indices_kernel", "inbounds_kernel", "validate_indices_kernel"
+    "create_horizontal_bilinear_interpolation_kernel",
+    "create_trilinear_interpolation_kernel",
+    "finite_indices_kernel",
+    "inbounds_kernel",
+    "validate_indices_kernel",
 ]
---
+++ src/offline_particles/kernels/interpolation.py
@@ -0,0 +1,51 @@
+"""Simple interpolation kernels for computing output for example."""
+
+import numpy as np
+import numpy.typing as npt
+
+from ..interpolation import trilinear_interpolation
+from ..kernel_tools import offset_indices_2D, offset_indices_3D
+from ..particle_kernel import ParticleKernel
+
+
+def create_trilinear_interpolation_kernel(field: str, particle_field: str | None = None, dtype: npt.DTypeLike = np.float64) ->ParticleKernel:
+    """Create a ParticleKernel that performs trilinear interpolation."""
+    if particle_field is None:
+        particle_field = field
+
+    def kernel_fn(
+        particle,
+        field_data,
+        field_off,
+    ):
+        idx = offset_indices_3D(particle["zidx"], particle["yidx"], particle["xidx"], field_off)
+        interp_value = trilinear_interpolation(idx, field_data)
+        particle[particle_field] = interp_value
+
+    return ParticleKernel(
+        kernel_fn,
+        particle_fields={"zidx": float, "yidx": float, "xidx": float, particle_field: dtype},
+        scalars=(),
+        simulation_fields=[field],
+    )
+
+def create_horizontal_bilinear_interpolation_kernel(field: str, particle_field: str | None = None, dtype: npt.DTypeLike = np.float64) ->ParticleKernel:
+    """Create a ParticleKernel that performs bilinear interpolation."""
+    if particle_field is None:
+        particle_field = field
+
+    def kernel_fn(
+        particle,
+        field_data,
+        field_off,
+    ):
+        idx = offset_indices_2D(particle["yidx"], particle["xidx"], field_off)
+        interp_value = trilinear_interpolation(idx, field_data)
+        particle[particle_field] = interp_value
+
+    return ParticleKernel(
+        kernel_fn,
+        particle_fields={"yidx": float, "xidx": float, particle_field: dtype},
+        scalars=(),
+        simulation_fields=[field],
+    )
